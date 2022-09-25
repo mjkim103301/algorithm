070489935afa78e74f1ec5a715ac9dd61edb63a4
Minji Kim <mjkim103301@gmail.com>
2022-09-25 15:19:22 +0900

    algo: 별찍기, 단순 for문

---
+++ java/BOJ2438_별찍기1.java
@@ -0,0 +1,16 @@
+package baekjoon.level_bronze;
+
+import java.util.Scanner;
+
+public class BOJ2438_별찍기1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

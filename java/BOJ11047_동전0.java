8290d45fa77c439fa7139cc68d2ea2e710bb62be
Minji Kim <mjkim103301@gmail.com>
2022-10-09 22:58:35 +0900

    algo: greedy

---
+++ java/BOJ11047_동전0.java
@@ -0,0 +1,35 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11047_동전0 {
+    static int N, K;
+    static int[] coins;
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        coins = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            int value = K / coins[i];
+            if (value > 0) {
+                answer += value;
+                K -= value * coins[i];
+            }
+        }
+        System.out.println(answer);
+    }
+}

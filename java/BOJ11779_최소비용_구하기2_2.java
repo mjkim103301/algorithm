48a9bde6984963fa57a9c52cf548bbf51e0ba5c8
Minji Kim <mjkim103301@gmail.com>
2022-12-28 22:28:34 +0900

    algo: 다익스트라 pq로 풀기 O( m * log(n))

--- java/BOJ11779_최소비용_구하기2_2.java
+++ java/BOJ11779_최소비용_구하기2_2.java
@@ -3,24 +3,27 @@ package baekjoon.gold;
 import java.io.*;
 import java.util.*;
 
-public class BOJ11779_최소비용_구하기2 {
+
+//pq로 풀기
+public class BOJ11779_최소비용_구하기2_2 {
     static int N, M;
 
     static class Node {
-        int from, to;
+        int to;
         int cost;
 
-        public Node(int from, int to, int cost) {
-            this.from = from;
+        public Node(int to, int cost) {
             this.to = to;
             this.cost = cost;
         }
     }
 
-    static List<Node> lines = new ArrayList<>();
+    static List<Node>[] graph;
     static List<Integer> path = new ArrayList<>();
     static Map<Integer, Integer> linkedMap = new HashMap<>();
-    static long[] distance;
+    static PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> (o1.cost - o2.cost));
+    static int[] distance;
+    static boolean[] visit;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -29,13 +32,18 @@ public class BOJ11779_최소비용_구하기2 {
 
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        distance = new long[N + 1];
+        distance = new int[N + 1];
+        graph = new ArrayList[N + 1];
+        visit = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            lines.add(new Node(from, to, cost));
+            graph[from].add(new Node(to, cost));
         }
 
         st = new StringTokenizer(br.readLine());
@@ -68,20 +76,22 @@ public class BOJ11779_최소비용_구하기2 {
     static void dijkstra(int from) {
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[from] = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 0; j < M; j++) {
-                run(lines.get(j));
-            }
-        }
-    }
+        pq.add(new Node(from, 0));
 
-    static void run(Node now) {
-        if (distance[now.from] == Integer.MAX_VALUE) {
-            return;
-        }
-        if (distance[now.from] + now.cost < distance[now.to]) {
-            distance[now.to] = distance[now.from] + now.cost;
-            addMap(now.from, now.to);
+        while(!pq.isEmpty()){
+            Node now=pq.poll();
+            if(visit[now.to]){
+                continue;
+            }
+            visit[now.to]=true;
+
+            for(Node next: graph[now.to]){
+                if(distance[now.to]+next.cost<distance[next.to]){
+                    distance[next.to]=distance[now.to]+next.cost;
+                    pq.add(new Node(next.to, distance[next.to]));
+                    addMap(now.to, next.to);
+                }
+            }
         }
     }
 
